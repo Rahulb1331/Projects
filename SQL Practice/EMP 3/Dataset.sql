fba74d0e714b546e5bb223974201daee119cb77b
Rahulb1331 <109972113+Rahulb1331@users.noreply.github.com>
2024-02-11 19:33:37 +0530

    Delete SQL Practice/EMP 3 directory

--- SQL Practice/EMP 3/Dataset.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM employee;
-SELECT * FROM bonus;
-SELECT * FROM department;
-SELECT * FROM project;
-SELECT * FROM project_participation;
-SELECT * FROM role;
-SELECT * FROM salarygrade;
-
--- SQL Practice/EMP 3/Description.sql
+++
@@ -1,7 +0,0 @@
-desc employee;
-desc project;
-desc project_participation;
-desc department;
-desc role;
-desc bonus;
-desc salarygrade;
