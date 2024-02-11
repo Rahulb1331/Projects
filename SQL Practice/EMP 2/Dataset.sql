537fde07621ebff08d7a356d6c3d1c1f1d378800
Rahulb1331 <109972113+Rahulb1331@users.noreply.github.com>
2024-02-11 19:32:40 +0530

    Delete SQL Practice/EMP 2 directory

--- SQL Practice/EMP 2/Dataset.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM bonus;
-SELECT * FROM department;
-SELECT * FROM employee;
-SELECT * FROM project;
-SELECT * FROM project_participation;
-SELECT * FROM role;
-SELECT * FROM salarygrade;
